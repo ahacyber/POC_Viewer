ff227b7b6b5fb7ca1aa62cdbd28ef3dabedc70b2
ahacyber <jm.kim@okestro.com>
2024-05-14 10:12:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ class App {
 
         const loader = new GLTFLoader();
 
-        loader.load("./POC_Viewer/data/character.glb", (gltf) => {
+        loader.load("../POC_Viewer/data/character.glb", (gltf) => {
             const model = gltf.scene;
             this._scene.add(model);
 
@@ -158,7 +158,7 @@ class App {
             // this._worldOctree.fromGraphNode(boxM);
         });
 
-        loader.load("./POC_Viewer/data/daejoen_step06_wall_test05.glb", (gltf) => {
+        loader.load("../POC_Viewer/data/daejoen_step06_wall_test05.glb", (gltf) => {
             const model = gltf.scene;
 
             this._scene.add(model);
